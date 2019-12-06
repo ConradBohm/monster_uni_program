b6532bf0a4b285661f61c3a07c00d57017fcd486
Conrad Bohm <CBohm@spartaglobal.com>
2019-12-06 14:10:15 +0000

    added data file to clean up main, made objects

---
+++ data.py
@@ -0,0 +1,26 @@
+from monster_classes import *
+from building_classes import *
+
+# Student List
+j_black = Student('James Black', 2, 'Yellow', '001', 'C')
+t_foolery = Student('Tom Foolery', 6, 'Greenish', '002', 'B')
+j_mcintyre = Student('Jess McIntyre', 4.5, 'Peach', '003', 'A*')
+b_jameson = Student('Beth Jameson', 2, 'Black and Blue', '004', 'A')
+
+# Teacher List
+m_rampling = Teacher('Mandy Rampling', 3.9, 'Orange', '100')
+j_johnson = Teacher('Jeremy Johnson', 3, 'White', '101')
+
+# Student Halls List
+room_1 = Halls('Main Campus', 1, 'B', j_mcintyre)
+room_2 = Halls('Main Campus', 2, 'B', t_foolery)
+room_3 = Halls('Main Campus', 3, 'B', b_jameson)
+room_4 = Halls('Main Campus', 4, 'B', j_black)
+
+# Lecture Theatre Building List
+lecture_1 = LectureTheatre('Main Campus', 10)
+lecture_2 = LectureTheatre('Main Campus', 20)
+
+# Workshop List
+scaring = SpookyWorkshop('Scaring', j_johnson, lecture_1)
+spook_science = SpookyWorkshop('Spook Science', m_rampling, lecture_2)
