b4905b34d9db5a1c0a3d6ec155b36595295d0b4b
Conrad Bohm <CBohm@spartaglobal.com>
2019-12-06 12:07:12 +0000

    added comments

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from functions import *
 
 print('---///--- Monster University Program ---\\\\\\---\n\n')
+
+# Main loop of the program. Contains the base menu and exit functionality
 while True:
     print('1 -- Students\n'
           '2 -- Rooms\n'
           '3 -- Workshops\n'
           '4 -- Staff\n'
           '5 -- Exit\n')
+
+    # Error handling, make sure user enters a number
     while True:
         try:
             choice = int(input('Pick a number from the menu options: '))
@@ -14,6 +18,7 @@ while True:
         except ValueError:
             print('\nNot a valid number, try again with a number digit from the menu.')
 
+    # Numbers relate to the menu options. Functions called are sub-menus
     if choice == 1:
         pass
 #        students()
@@ -29,5 +34,6 @@ while True:
     elif choice == 5:
 #        finish()
         break
+    # Else to return to the top of the loop if user didn't use a number 1-5
     else:
         print('We don\'t have that many options! Try a number on the menu.')
