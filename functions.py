78cfb38561af17dc53d17942ea4a40838df93300
Conrad Bohm <CBohm@spartaglobal.com>
2019-12-06 15:43:14 +0000

    added add_skill method to monster class, and adding and checking skills into student functions

--- functions.py
+++ functions.py
@@ -17,7 +17,8 @@ def students():
         print('\nStudents menu:\n'
               '1 -- View Student Details\n'
               '2 -- View Student List\n'
-              '3 -- Back to Main Menu\n')
+              '3 -- Add Skill to Student\n'
+              '4 -- Back to Main Menu\n')
 
         choice = menu_input()
 
@@ -26,6 +27,8 @@ def students():
         elif choice == 2:
             student_list()
         elif choice == 3:
+            add_skill()
+        elif choice == 4:
             break
         else:
             print('We don\'t have that many options! Try a number on the menu.')
@@ -40,7 +43,8 @@ def view_students():
                   f'No. of Legs: {student.leg_number}\n'
                   f'Colour: {student.colour}\n'
                   f'ID: {student.id}\n'
-                  f'Grade: {student.grade}')
+                  f'Grade: {student.grade}\n'
+                  f'Skills: {student.skills}')
             found = True
             break
         else:
@@ -53,3 +57,22 @@ def view_students():
 def student_list():
     for student in stud_list:
         print(student.name)
+
+
+def add_skill():
+    found = False
+    check = input('Which student do you wish to add a skill for? ').lower()
+    skill = input('What is the skill you wish to add? ')
+    for student in stud_list:
+        if check in student.name.lower():
+            student.add_skill(skill)
+            found = True
+            break
+
+        else:
+            continue
+
+    if not found:
+        print('That name wasn\'t in our records! Check your spelling or try a different name.')
+    else:
+        print('Update made.')
--- monster_classes.py
+++ monster_classes.py
@@ -5,6 +5,9 @@ class Monster:
         self.leg_number = legs
         self.colour = colour
 
+    def add_skill(self, skill):
+        self.skills.append(skill)
+
 
 class Student(Monster):
     def __init__(self, name, legs, colour, student_id, grade):
@@ -12,8 +15,10 @@ class Student(Monster):
         self.id = student_id
         self.grade = grade
 
+
 class Teacher(Monster):
-    def __init__(self, name, legs, colour, teacher_id, date):
+    def __init__(self, name, legs, colour, teacher_id):
         super().__init__(name, legs, colour)
         self.id = teacher_id
-        self.start_date = date
+
+
